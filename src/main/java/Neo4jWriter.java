d40b81356c8a40aa97a8ddcfd355c514b7cc3421
Said Bouigherdaine <ibouig@gmail.com>
2017-12-15 23:35:54 +0000

    Query timeline of tweet's user, replayed user & mentions users

--- src/main/java/Neo4jWriter.java
+++ src/main/java/Neo4jWriter.java
@@ -18,15 +18,138 @@ import static java.util.stream.Collectors.toList;
  */
 class Neo4jWriter {
     static String STATEMENT = "UNWIND {tweets} AS t\n" +
-            "   WITH t\n" +
-            "       \n" +
-            "   \n" +
-            "   WHERE t.id is not null\n" +
-            "   MERGE (tweet:Tweet {id:t.id})\n" +
-            "   SET\n" +
-            "       tweet.text = t.text,\n" +
-            "       tweet.createdAt = t.createdAt,\n" +
-            "       tweet.source = t.source"
+            "    WITH t,\n" +
+            "        t.contributorsIDs as contributors,\n" +
+            "        t.retweetedStatus AS retweetedStatus,\n" +
+            "        t.userMentionEntities AS userMentionEntities,\n" +
+            "        t.urlEntities AS urlEntities,\n" +
+            "        t.hashtagEntities AS hashtagEntities,\n" +
+            "        t.mediaEntities AS mediaEntities,\n" +
+            "        t.symbolEntities AS symbolEntities,\n" +
+            "        t.user AS u\n" +
+            "    WHERE t.id is not null\n" +
+            "\n" +
+            "    MERGE (tweet:Tweet {id:t.id})\n" +
+            "    SET\n" +
+            "        tweet.text = t.text,\n" +
+            "        tweet.createdAt = t.createdAt,\n" +
+            "        tweet.source = t.source,\n" +
+            "        tweet.isTruncated = t.isTruncated,\n" +
+            "        tweet.isFavorited = t.isFavorited,\n" +
+            "        tweet.isRetweeted = t.isRetweeted,\n" +
+            "        tweet.favoriteCount = t.favoriteCount,\n" +
+            "        tweet.retweetCount = t.retweetCount,\n" +
+            "        tweet.isPossiblySensitive = t.isPossiblySensitive,\n" +
+            "        tweet.lang = t.lang,\n" +
+            "        tweet.currentUserRetweetId = t.currentUserRetweetId,\n" +
+            "        tweet.quotedStatusId = t.quotedStatusId\n" +
+            "\n" +
+            "    MERGE (user:User {screenName:u.screenName})\n" +
+            "    SET\n" +
+            "        user.name = u.name,\n" +
+            "        user.id = u.id,\n" +
+            "        user.location = u.location,\n" +
+            "        user.isProtected = u.isProtected,\n" +
+            "        user.followersCount = u.followersCount,\n" +
+            "        user.friendsCount = u.friendsCount,\n" +
+            "        user.createdAt = u.createdAt,\n" +
+            "        user.favouritesCount = u.favouritesCount,\n" +
+            "        user.utcOffset = u.utcOffset,\n" +
+            "        user.lang = u.lang\n" +
+            "\n" +
+            "    MERGE (user)-[:POSTED]->(tweet)\n" +
+            "    FOREACH (h IN hashtagEntities |\n" +
+            "        MERGE (tag:Tag {text:LOWER(h.text)})\n" +
+            "        MERGE (tag)<-[:TAGGED]-(tweet)\n" +
+            "    )\n" +
+            "    FOREACH (u IN [u IN urlEntities WHERE u.expandedURL IS NOT NULL] |\n" +
+            "        MERGE (url:Link {url:u.expandedURL})\n" +
+            "        MERGE (tweet)-[:LINKED]->(url)\n" +
+            "    )\n" +
+            "    FOREACH (m IN [m IN mediaEntities WHERE m.id IS NOT NULL] |\n" +
+            "        MERGE (media:Media {id:m.id})\n" +
+            "        ON CREATE SET\n" +
+            "          media.mediaURL= m.mediaURL\n" +
+            "        MERGE (tweet)-[:CONTAINED]->(media)\n" +
+            "    )\n" +
+            "    FOREACH (m IN userMentionEntities |\n" +
+            "        MERGE (mentioned:User {screenName:m.screenName})\n" +
+            "        ON CREATE SET\n" +
+            "          mentioned.name = m.name,\n" +
+            "          mentioned.id = m.id\n" +
+            "        MERGE (tweet)-[:MENTIONED]->(mentioned)\n" +
+            "    )\n" +
+            "    FOREACH (r IN [r IN [t.inReplyToStatusId] WHERE r IS NOT NULL AND r <> -1 ] |\n" +
+            "        MERGE (reply_tweet:Tweet {id:r})\n" +
+            "        MERGE (tweet)-[:REPLIED_TO]->(reply_tweet)\n" +
+            "    )\n" +
+            "    \n" +
+            "\n" +
+            "    FOREACH (r IN [r IN [retweetedStatus] WHERE r IS NOT NULL AND r <> -1 ] |\n" +
+            "    \n" +
+            "    \n" +
+            "        MERGE (retweet_tweet:Tweet {id:r.id})\n" +
+            "        SET\n" +
+            "            retweet_tweet.text = r.text,\n" +
+            "            retweet_tweet.createdAt = r.createdAt,\n" +
+            "            retweet_tweet.source = r.source,\n" +
+            "            retweet_tweet.isTruncated = r.isTruncated,\n" +
+            "            retweet_tweet.isFavorited = r.isFavorited,\n" +
+            "            retweet_tweet.isRetweeted = r.isRetweeted,\n" +
+            "            retweet_tweet.favoriteCount = r.favoriteCount,\n" +
+            "            retweet_tweet.retweetCount = r.retweetCount,\n" +
+            "            retweet_tweet.isPossiblySensitive = r.isPossiblySensitive,\n" +
+            "            retweet_tweet.lang = r.lang,\n" +
+            "            retweet_tweet.currentUserRetweetId = r.currentUserRetweetId,\n" +
+            "            retweet_tweet.quotedStatusId = r.quotedStatusId\n" +
+            "        \n" +
+            "        MERGE (tweet)-[:RETWEETED]->(retweet_tweet)\n" +
+            "\n" +
+            "        MERGE (user:User {screenName:r.user.screenName})\n" +
+            "        SET\n" +
+            "            user.name = r.user.name,\n" +
+            "            user.id = r.user.id,\n" +
+            "            user.location = r.user.location,\n" +
+            "            user.isProtected = r.user.isProtected,\n" +
+            "            user.followersCount = r.user.followersCount,\n" +
+            "            user.friendsCount = r.user.friendsCount,\n" +
+            "            user.createdAt = r.user.createdAt,\n" +
+            "            user.favouritesCount = r.user.favouritesCount,\n" +
+            "            user.utcOffset = r.user.utcOffset,\n" +
+            "            user.lang = r.user.lang\n" +
+            "\n" +
+            "        MERGE (user)-[:POSTED]->(retweet_tweet)\n" +
+            "\n" +
+            "        FOREACH (h IN r.hashtagEntities |\n" +
+            "            MERGE (tag:Tag {text:LOWER(h.text)})\n" +
+            "            MERGE (tag)<-[:TAGGED]-(retweet_tweet)\n" +
+            "        )\n" +
+            "        FOREACH (u IN [u IN r.urlEntities WHERE u.expandedURL IS NOT NULL] |\n" +
+            "            MERGE (url:Link {url:u.expandedURL})\n" +
+            "            MERGE (retweet_tweet)-[:LINKED]->(url)\n" +
+            "        )\n" +
+            "        FOREACH (m IN [m IN r.mediaEntities WHERE m.id IS NOT NULL] |\n" +
+            "            MERGE (media:Media {id:m.id})\n" +
+            "            ON CREATE SET\n" +
+            "              media.mediaURL= m.mediaURL\n" +
+            "            MERGE (retweet_tweet)-[:CONTAINED]->(media)\n" +
+            "        )\n" +
+            "        FOREACH (m IN r.userMentionEntities |\n" +
+            "            MERGE (mentioned:User {screenName:m.screenName})\n" +
+            "            ON CREATE SET\n" +
+            "              mentioned.name = m.name,\n" +
+            "              mentioned.id = m.id\n" +
+            "            MERGE (retweet_tweet)-[:MENTIONED]->(mentioned)\n" +
+            "        )\n" +
+            "        FOREACH ( a IN [ a IN [r.inReplyToStatusId] WHERE a IS NOT NULL AND a <> -1 ] |\n" +
+            "            MERGE (reply_tweet_tweet:Tweet {id:a})\n" +
+            "            MERGE (reply_tweet)-[:REPLIED_TO]->(reply_tweet_tweet)\n" +
+            "        )\n" +
+            "        \n" +
+            "\n" +
+            "        \n" +
+            "    \n" +
+            "    )"
             ;
     private Driver driver;
 
@@ -39,10 +162,12 @@ class Neo4jWriter {
     public void init() {
         try (Session session = driver.session()) {
             session.run("CREATE CONSTRAINT ON (t:Tweet) ASSERT t.id IS UNIQUE");
-            session.run("CREATE CONSTRAINT ON (u:User) ASSERT u.screen_name IS UNIQUE");
-            session.run("CREATE CONSTRAINT ON (t:Tag) ASSERT t.name IS UNIQUE");
+            session.run("CREATE CONSTRAINT ON (u:User) ASSERT u.screenName IS UNIQUE");
+            session.run("CREATE CONSTRAINT ON (t:Tag) ASSERT t.text IS UNIQUE");
             session.run("CREATE CONSTRAINT ON (l:Link) ASSERT l.url IS UNIQUE");
             session.run("CREATE CONSTRAINT ON (p:Place) ASSERT p.id IS UNIQUE");
+            session.run("CREATE CONSTRAINT ON (m:Media) ASSERT m.id IS UNIQUE");
+
         }
     }
 
@@ -54,35 +179,26 @@ class Neo4jWriter {
         //System.out.println("TWEET "+tweets.get(0) );
         while (retries > 0) {
             try (Session session = driver.session()) {
-                Gson gson = new GsonBuilder().create()
-                        ;
-
-
+                Gson gson = new GsonBuilder().create();
 
                 List<Map> statuses = tweets.stream().map((s) -> gson.fromJson(s, Map.class)).collect(toList());
                 long time = System.nanoTime();
 
                 ResultSummary result = session.run(STATEMENT, Values.parameters("tweets", statuses)).consume();
                 int created = result.counters().nodesCreated();
-                //System.out.println("STATUS "+statuses.get(0));
 
 
                 System.out.println(created+" nodes in "+ TimeUnit.NANOSECONDS.toMillis(System.nanoTime()-time)+" ms");
                 System.out.flush();
 
-                //System.out.println("STATUS "+statuses.get(0)+"," );
-
                 if(created == 0 ) {
                     System.out.println(" ############################## " +statuses.get(0).get("id"));
-
-
 
                 }
 
                 //System.out.println(tweets.get(0));
                 //System.out.println(tweets.get(1));
 
-                //System.out.println(statuses.get(0));
                 //System.out.println(statuses.get(1));
 
                 return created;
--- src/main/java/TwitterStreamer.java
+++ src/main/java/TwitterStreamer.java
@@ -30,13 +30,13 @@ import static java.util.Arrays.asList;
 
 public class TwitterStreamer {
 
-    private static final int BATCH = 100;
+    private static final int BATCH = 2;
     int maxReads = 1;
 
     private static Neo4jWriter writer;
     private static ExecutorService service;
     private static List<String> buffer;
-
+    private static Twitter twitter;
 
 
 
@@ -76,20 +76,64 @@ public class TwitterStreamer {
                 .setOAuthAccessToken("2736990925-jbottSBXTRZYEkttWEeT9sOfgpmfQDDQBcOX8xP")
                 .setOAuthAccessTokenSecret("qiVi76t9P82CvA7gboJ1r17Mech3kbGrdan0YQenLVE5q")
                 .setDebugEnabled(true);
+
+        ConfigurationBuilder cb_ = new ConfigurationBuilder();
+        cb_.setDebugEnabled(true)
+                .setOAuthConsumerKey("I8KdkJQ3E8YHL1zJZgz1FLJOM")
+                .setOAuthConsumerSecret("gjzGa0XVKxcdJfmE9A5xaRyxGu1dqgJa8GoW4r1DqXMltqcbrS")
+                .setOAuthAccessToken("1635806574-9YMLOTPr5ELDE0NXpASPv3XscjUEGIFrNuZAy0Z")
+                .setOAuthAccessTokenSecret("wP7oAOV1rcZzvlzg1t6DZvMPH9mK5Xg3iHrW2s6AuHlVM")
+                .setDebugEnabled(true);
+
+
+
         //TwitterFactory tf = new TwitterFactory(cb.build());
         //Twitter twitter = tf.getInstance();
 
+        writer = new Neo4jWriter(NEO4J_URL);
+        writer.init();
 
         StatusListener listener = new StatusListener(){
             public void onStatus(Status status) {
                 //System.out.println(copyToJsonString(status));
                 //System.out.println(TwitterObjectFactory.getRawJSON( status ));
                 buffer.add(copyToJsonString(status));
-                //tweets.add(copyToJsonString(status));
-                //System.out.println(copyToJsonString(status)+",");
+                //get tweets timeline
+                //System.out.println(status.getUser().getScreenName());
+                try {
+                    List<Status> statusList = TwitterStreamer.twitter.getUserTimeline(status.getUser().getScreenName());
+                    if(status.getInReplyToScreenName()!=null)
+                        statusList.addAll(TwitterStreamer.twitter.getUserTimeline(status.getInReplyToScreenName()));
+                    if(status.getUserMentionEntities().length!=0){
+                        for (UserMentionEntity u:status.getUserMentionEntities()) {
+                            statusList.addAll(TwitterStreamer.twitter.getUserTimeline(u.getScreenName()));
+                        }
+                    }
+
+                    for (Status s:statusList) {
+                        buffer.add(copyToJsonString(s));
+                    }
+
+
+
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+
+
+                //get tweets timeline of user whome we're replaying to
+                status.getInReplyToScreenName();
+                //get tweets timeline of user we mentioned in our tweet
+                //status.getUserMentionEntities()[0].getScreenName();
+
+
+
+
                 if (buffer.size() >= BATCH) {
                     List<String> tweets = buffer;
                     buffer = new ArrayList<>(BATCH);
+                    System.out.println(tweets);
+
                     service.submit(() -> writer.insert(tweets,3));
 
                 }
@@ -117,9 +161,11 @@ public class TwitterStreamer {
         TwitterStream twitterStream = twitterStreamFactory.getInstance();
         twitterStream.addListener(listener);
 
+        TwitterFactory tf = new TwitterFactory(cb_.build());
+        twitter = tf.getInstance();
+        //System.out.println(twitter.getUserTimeline());
 
-        writer = new Neo4jWriter(NEO4J_URL);
-        writer.init();
+
         int numProcessingThreads = Math.max(1,Runtime.getRuntime().availableProcessors() - 1);
         service = Executors.newFixedThreadPool(numProcessingThreads);
 
@@ -130,7 +176,12 @@ public class TwitterStreamer {
 
 
         // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
-        twitterStream.sample();
+        //twitterStream.sample();
+        String[] s = {"palestine","trump"};
+        twitterStream.filter(new FilterQuery().track(s));
+
+
+
 
         //Output to a file or terminal
 
